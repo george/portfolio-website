42208e9b0fd5cdbc24a0f10fa40d8e6c1ce354b1
George <sam.polshek@gmail.com>
2023-02-17 03:37:59 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from './components/Hero';
+import './index.css'
+
+const App = () => {
+  return (
+      <div className="container">
+          <Hero/>
+      </div>
+  );
+}
+
+export default App;
