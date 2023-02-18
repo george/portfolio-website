d050b1fd47f34087341f42f269b72a113c770a40
George <sam.polshek@gmail.com>
2023-02-17 20:37:00 -0600

    Added title component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Title from "./Title";
 
 const Hero = () => {
     return (
         <div className="center">
-
+            <Title />
         </div>
     )
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Title = () => {
+    return <h1>Hi, I'm George.</h1>
+}
+
+export default Title;
